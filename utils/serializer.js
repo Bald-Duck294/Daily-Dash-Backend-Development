290e9be81ef03732b9c2f17097054d5e4f34c917
Goolu Moolu <goolumoolu294@gmail.com>
2026-06-25 00:51:30 +0530

    updated firebase

--- utils/serializer.js
+++ utils/serializer.js
@@ -18,32 +18,31 @@
 //   return obj;
 // }
 
-
 // utils/serializer.js
 export function serializeBigInt(obj) {
   if (obj === null || obj === undefined) {
     return obj;
   }
-  
-  if (typeof obj === 'bigint') {
+
+  if (typeof obj === "bigint") {
     return obj.toString();
   }
-  
+
   if (obj instanceof Date) {
     return obj.toISOString();
   }
-  
+
   if (Array.isArray(obj)) {
     return obj.map(serializeBigInt);
   }
-  
-  if (typeof obj === 'object') {
+
+  if (typeof obj === "object") {
     const serialized = {};
     for (const [key, value] of Object.entries(obj)) {
       serialized[key] = serializeBigInt(value);
     }
     return serialized;
   }
-  
+
   return obj;
 }
