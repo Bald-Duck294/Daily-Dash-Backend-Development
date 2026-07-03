00ea992d32d4d9da34aee4bdbc0d04ad8e1de351
Goolu Moolu <goolumoolu294@gmail.com>
2026-07-03 14:28:40 +0530

    fixed login routes

--- routes/loginRouts.js
+++ routes/loginRouts.js
@@ -1,14 +1,16 @@
-import express from "express";
-import bcrypt from "bcryptjs";
-import prisma from "../config/prismaClient.mjs";
-import {
-  loginUser,
-  registerUser,
-  resetPassword,
-} from "../controller/authController.js";
-const loginRoute11 = express.Router();
+// import express from "express";
+// import bcrypt from "bcryptjs";
+// import prisma from "../config/prismaClient.mjs";
+// import {
+//   loginUser,
+//   registerUser,
+//   resetPassword,
+//   getOnboardingStatus,
+// } from "../controller/authController.js";
+// const loginRoute11 = express.Router();
 
-loginRoute11.post("/login", loginUser);
-loginRoute11.post("/register", registerUser);
-loginRoute11.post("/reset-password", resetPassword);
-export default loginRoute11;
+// loginRoute11.post("/login", loginUser);
+// loginRoute11.post("/register", registerUser);
+// loginRoute11.post("/reset-password", resetPassword);
+// loginRoute11.get("/onboarding-status", verifyToken, getOnboardingStatus);
+// export default loginRoute11;
