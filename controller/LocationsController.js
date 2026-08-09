344f1d927a82b639631f5a9f962e72ed008e1d70
Goolu Moolu <goolumoolu294@gmail.com>
2026-08-10 02:21:18 +0530

    feat: add LocationsController with utility for validating and formatting operation schedules

--- controller/LocationsController.js
+++ controller/LocationsController.js
@@ -4,7 +4,7 @@ import db from "../db.js";
 import qrcode from "qrcode";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-import archiver from "archiver";
+import { ZipArchive } from "archiver";
 // import RBACFilterService from "../services/rbacFilterService.js";
 import RBACFilterService from "../utils/rbacFilterService.js";
 
@@ -2496,7 +2496,7 @@ export const downloadLocationQRs = async (req, res) => {
     } 
     
     if (downloadType === 'usage_category_only' || downloadType === 'both') {
-      const archive = archiver('zip', { zlib: { level: 9 } });
+      const archive = new ZipArchive({ zlib: { level: 9 } });
       
       archive.on('error', (err) => {
         throw err;
